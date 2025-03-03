f8057dad51bb19e66a079734f9a4f16bee3442dc
VitorMaximoOC <vitormaximodoc@gmail.com>
2025-03-03 19:43:51 +0000

    Cartas 1 e 2 e comentários

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,11 +3,12 @@
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+
 
 int main() {
+
+    //Variáveis da carta 1: informações sobre o primeiro estado e cidade.
+
     char estado1[50];
     char cidade1[50];
     char codigo1[50];
@@ -15,6 +16,8 @@ int main() {
     float area1;
     float pib1;
     int pontosTuristicos1;
+
+    //Variáveis da carta 2: informações sobre o segundo estado e cidade.
 
     char estado2[50];
     char cidade2[50];
@@ -24,10 +27,8 @@ int main() {
     float pib2;
     int pontosTuristicos2;
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
+    //Inserção de dados da carta 1 pelo jogador.
+
     printf("Coloque os dados da carta 1. \n");
     printf("Estado: ");
     scanf("%s", &estado1);
@@ -44,12 +45,16 @@ int main() {
     printf("Pontos Turísticos: ");
     scanf("%d", &pontosTuristicos1);
 
+    //Resultado das informações inseridas pelo jogador sobre a carta 1.
+
     printf("\nEsses são os dados da carta 1:");
     printf("\nEstado: %s\n", estado1);
     printf("Cidade: %s\n", cidade1);
     printf("Código: %s\n", codigo1);
     printf("População: %d - Área: %.2f Km² - PIB: R$ %.2f\n", populacao1, area1, pib1);
     printf("Pontos Turísticos: %d\n\n", pontosTuristicos1);
+
+    //Inserção de dados da carta 2 pelo jogador.
 
     printf("Coloque os dados da carta 2. \n");
     printf("Estado: ");
@@ -67,20 +72,14 @@ int main() {
     printf("Pontos Turísticos: ");
     scanf("%d", &pontosTuristicos2);
 
+    //Resultado das informações inseridas pelo jogador sobre a carta 2.
+
     printf("\nEsses são os dados da carta 2:");
     printf("\nEstado: %s\n", estado2);
     printf("Cidade: %s\n", cidade2);
     printf("Código: %s\n", codigo2);
     printf("População: %d - Área: %.2f Km² - PIB: R$ %.2f\n", populacao2, area2, pib2);
     printf("Pontos Turísticos: %d", pontosTuristicos2);
-    printf("\n\nFim de jogo!");
-
-
-
-
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
     return 0;
 }
